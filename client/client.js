49f25f5a5b09201658ffaf61ffcffb8845234044
ANDRAULT Chloé <chloe.andrault@hetic.net>
2016-12-13 11:21:31 +0100

    Ordre des messages

--- client/client.js
+++ client/client.js
@@ -1,34 +1,35 @@
 if (Meteor.isClient) {
-       Messages = new Meteor.Collection('messages');
+  Messages = new Meteor.Collection('messages');
 
-       Deps.autorun(function() {
-         Meteor.subscribe('messages', {
-               onReady : function() {
-                   Session.set("active", true); // pour dire que c'est synchronisé (donc on peut commencer à rafraichir le template)
-               }
-         });
-       });
+  Deps.autorun(function() {
+    Meteor.subscribe('messages', {
+      onReady : function() {
+        Session.set("active", true); 
+      }
+    });
+  });
 
-     Template.cosmochat.events({
-        'submit form': function(event) {
-           event.preventDefault();
-           var post = {
-               pseudo : $(event.target).find('[name=pseudo]').val(),
-               message : $(event.target).find('[name=message]').val()
-           }
-           if ( (post.message != "") && (post.pseudo != "") ) {
-               Meteor.call("ajouteMessage", post);
-           }
-       }
-     });
+  Template.cosmochat.events({
+    'submit form': function(event) {
+      event.preventDefault();
+      var post = {
+        pseudo : $(event.target).find('[name=pseudo]').val(),
+        message : $(event.target).find('[name=message]').val()
+      }
+      if ( (post.message != "") && (post.pseudo != "") ) {
+        Meteor.call("ajouteMessage", post);
+      }
+    }
+  });
 
-      Template.cosmochat.helpers({
-          derniersMessages : function() {
-                if (Session.get("active")) {
-                    return Messages.find({}, {sort : {time : -1}, limit : 500});
-                } else {
-                    return [];
-                }
-           }
-      });
-   }
+  Template.cosmochat.helpers({
+    derniersMessages : function() {
+      if (Session.get("active")) {
+        return Messages.find({}, {sort : {time : 1}, limit : 200});
+      } 
+      else {
+        return [];
+      }
+    }
+  });
+}
--- server/server.js
+++ server/server.js
@@ -1,23 +1,21 @@
 if (Meteor.isServer) {
 
-      Messages = new Meteor.Collection('messages');
+  Messages = new Meteor.Collection('messages');
 
-      Meteor.methods ({
-          ajouteMessage : function (post) {
-              var timestamp = new Date().toTimeString();
-              Messages.insert({
-                  pseudo : post.pseudo,
-                  message : post.message,
-                  time : timestamp
-              });
-          }
+  Meteor.methods ({
+    ajouteMessage : function (post) {
+      var timestamp = new Date().toTimeString();
+      Messages.insert({
+        pseudo : post.pseudo,
+        message : post.message,
+        time : timestamp
       });
+    }
+  });
 
-      Meteor.publish('messages', function(salon) {
-         return Messages.find( {}, {sort : {time : -1}, limit : 500} );
-      });
+  Meteor.publish('messages', function(salon) {
+   return Messages.find( {}, {sort : {time : -1}, limit : 200} );
+  });
 
-      Meteor.startup(function () {
-        // code to run on server at startup
-      });
-  }
+  Meteor.startup(function () { });
+}
