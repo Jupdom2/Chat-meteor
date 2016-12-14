d0a48885eaa7d966a386e6c84158e66e129fc5e8
ANDRAULT Chloé <chloe.andrault@hetic.net>
2016-12-14 16:47:30 +0100

    update

---
+++ README.md
@@ -0,0 +1,3 @@
+
+Nous avons créée un chat fonctionnel avec meteor.
+Comme amélioration nous avons souhaité créer un système de channels comme Slack.
--- client/client.js
+++ client/client.js
@@ -1,3 +1,5 @@
+import{Session} from "meteor/session";
+
 if (Meteor.isClient) {
   Messages = new Meteor.Collection('messages');
 
@@ -18,10 +20,16 @@ if (Meteor.isClient) {
         channel : $(event.target).find('[name=chanel]').val()
       }
 
+      // **** RESET INFOS ****
+
       $(event.target).find('[name=pseudo]').val(" "),
       $(event.target).find('[name=message]').val(" ")
-      
+
+      // **** TEST ****
+
       console.log(post)
+
+      // **** AJOUT DU MESSAGE ****
 
       if ( (post.message != "") && (post.pseudo != "") ) {
         Meteor.call("ajouteMessage", post);
@@ -30,6 +38,8 @@ if (Meteor.isClient) {
       }
     }
   });
+
+  // **** CHANEL 1 ****
 
   Template.chanel1.helpers({
     MessagesChan1 : function() {
@@ -41,7 +51,32 @@ if (Meteor.isClient) {
       }
     }
   });
+
+  // **** CHANEL 2 ****
+
+  Template.chanel2.helpers({
+    MessagesChan2 : function() {
+      if (Session.get("active")) {
+        return Messages.find({ chanel:2}, {sort : {time : 1}, limit : 200});
+      } 
+      else {
+        return [];
+      }
+    }
+  });
+
+  // **** CHANEL 3 ****
+
+  Template.chanel3.helpers({
+    MessagesChan3 : function() {
+      if (Session.get("active")) {
+        return Messages.find({ chanel:3}, {sort : {time : 1}, limit : 200});
+      } 
+      else {
+        return [];
+      }
+    }
+  });
 }
 
 
-// ///// CHANEL /////
